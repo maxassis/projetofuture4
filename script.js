af8e5d71b3d3d5787a763777db7d86d946a74991
max.assis <58948287+maxassis@users.noreply.github.com>
2020-02-24 17:57:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function apertar() {
-  var email = document.getElementById('caixa')
+function apertarbotao() {
+  var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
   var mensagem = document.getElementById('texto')
-  alert(`O email ${email.value} foi enviado`)
-  mensagem.innerHTML = `Projeto Seletivo Future4 <br><br><font color="#cd4d00"><u> Bem vindo ${email.value} ! </font></u>`
+  if (reg.test(email.value)) {
+    alert(`O email ${email.value} foi enviado`)
+    mensagem.innerHTML = `Projeto Seletivo Future4 <br><br><font color="#cd4d00"><u> Bem vindo ${email.value} ! </font></u>`
+  } else {
+    alert("Por favor, digite um email valido")
+  }
 }
